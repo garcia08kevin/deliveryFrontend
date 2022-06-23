6d0d678b6eb9bbf3c8a0efa9c3dccb97bee71084
garcia08kevin <kevin.garcia.aviles@udlae.udu.ec>
2022-06-23 17:47:11 -0500

    actualizacion

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ let app = express();
 app.use(express.static(__dirname+'/dist/angular-biblioteca'));
 
 app.get('/*', (req, resp) =>{
-    resp.sendFile(__dirname+'/dist/angular-biblioteca/index.html');
+    resp.sendFile(__dirname+'./angular-biblioteca/dist/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
