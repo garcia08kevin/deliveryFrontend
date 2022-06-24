473c25f15e9e1793f2f4a69aae692b9cf8180d14
garcia08kevin <kevin.garcia.aviles@udlae.udu.ec>
2022-06-23 21:32:00 -0500

    actualizacion

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ let express = require('express');
 
 let app = express();
 
-app.use(express.static(path.join(__dirname, 'dist','app_name')));
-res.sendFile(path.join(__dirname,'dist','app_name','index.html'));
+app.use(express.static(path.join(__dirname, 'dist','angular-biblioteca')));
+res.sendFile(path.join(__dirname,'dist','angular-biblioteca','index.html'));
 
 app.listen(process.env.PORT || 8080);
